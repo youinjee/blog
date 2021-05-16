a11faa1b12e4b9ee4e65867f23fe01bc6345808d
youinjee <dbdlswl3235@gmail.com>
2021-05-16 21:18:06 +0900

    upload files

---
+++ blogproject/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+import blogapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blogapp.views.home, name="home"),
+    path('blog/<int:blog_id>',blogapp.views.detail, name="detail"),
+]
